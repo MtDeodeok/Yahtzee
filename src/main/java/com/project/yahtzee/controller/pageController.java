53572cf1ec5ec1c1979a725b2e0174ee40dbc13f
더덕 <amdusias9109@gmail.com>
2022-03-14 12:44:21 +0900

    #8 파일 위치 조정

--- src/main/java/com/project/yahtzee/controller/pageController.java
+++ src/main/java/com/project/yahtzee/controller/pageController.java
@@ -3,36 +3,37 @@ package com.project.yahtzee.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.project.yahtzee.service.MatchService;
+import com.project.yahtzee.service.MessageBoardService;
 import com.project.yahtzee.util.Dice;
 import com.project.yahtzee.util.chat.WebSocketHandler;
 import com.project.yahtzee.util.gameRoom.GameRoom;
 import com.project.yahtzee.util.gameRoom.GameUser;
 import com.project.yahtzee.util.gameRoom.RoomManager;
 import com.project.yahtzee.vo.MemberVO;
-import com.project.yahtzee.vo.YahtzeeBoardVO;
+import com.project.yahtzee.vo.MessageBoardVO;
 
 import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
-public class pageController {
+public class PageController {
 	
 	GameUser gameUser = new GameUser();
 	Dice dice = new Dice();
 	WebSocketHandler handler = new WebSocketHandler();
 	RoomManager roomManager = new RoomManager(); 
-	private final MatchService matchservice;
 	List<String> roomList = new ArrayList<String>();
-	
+	private final MatchService matchservice;
+	private final MessageBoardService messageBoardService;
 	
 	@GetMapping("waitingRoom")
 	public void waitingRoom(Model model) {
@@ -63,10 +64,6 @@ public class pageController {
 	public void ranking(Model model) {
 		model.addAttribute("ranking", matchservice.ranking());
 	}
-	
-	@GetMapping("messageBoard")
-	public void messageBoard() {
-		
-	}
 
+	
 }
